e0c9d33b884a60beac311895e4cf512a4d93c8cc
Moumen <Moumen@Moumen-PC>
2016-09-16 13:46:54 +0200

    first commit and push moumen jamil

---
+++ maventest/src/test/java/mymaventest/TestmavenDemo.java
@@ -0,0 +1,24 @@
+package mymaventest;
+
+
+			
+	import org.testng.Assert;		
+	import org.testng.annotations.Test;	
+			
+			
+	public class TestmavenDemo {	
+				
+			@Test				
+			public void test_to_pass() {	
+								 
+				Assert.assertEquals(1,1); 		
+			}	
+			@Test				
+			public void test_to_fail() {	
+								 
+				Assert.assertEquals(1,2); 		
+			}	
+				
+	}	
+
+
