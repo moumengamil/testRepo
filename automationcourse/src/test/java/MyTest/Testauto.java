6c82f937345984cfc1d2d66d29dd903b90fb8482
Moumen <Moumen@Moumen-PC>
2016-09-16 20:46:10 +0200

    hello it's my first commit

---
+++ automationcourse/src/test/java/MyTest/Testauto.java
@@ -0,0 +1,26 @@
+package MyTest;
+import org.testng.Assert;		
+import org.testng.annotations.Test;	
+
+public class Testauto {
+	
+	
+			
+			
+		
+				
+			@Test				
+			public void test_to_pass() {	
+								 
+				Assert.assertEquals(1,1); 		
+			}	
+			@Test				
+			public void test_to_fail() {	
+								 
+				Assert.assertEquals(1,5); 		
+			}	
+				
+		
+	
+
+}
